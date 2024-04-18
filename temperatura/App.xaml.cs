c01c1b6706589e226466ad8de886321a6b553165
maria eduarda <mariaeduardakonevaliki@gmail.com>
2024-04-18 10:54:23 -0300

    dando inicio ao projeto 18/04

---
+++ temperatura/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace temperatura;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ temperatura/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace temperatura;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
